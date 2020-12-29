f663899c3744eb95a4244f3f16c47f1c8d2acb74
Timothy Fisher <timothyf@gmail.com>
2020-12-29 16:00:12 -0500

    updated resume format

---
+++ source/tags.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register ActsAsTaggableOn::Tag, as: "Tag" do
+
+  menu parent: "Product Data"
+
+
+  # permit_params :name, :user_id, :source_user_id, :source_username, :access_secret,
+  #               :access_token, :request_token, :request_secret, :verification_url,
+  #               :active, :data_source_type_id
+
+  # index do
+  #   selectable_column
+  #   id_column
+  #   column  :receipt_id
+  #   column  :shop
+  #   column  :customer
+  #   column  :created_at
+  #   column  :updated_at
+  #   actions
+  # end
+
+
+
+end
